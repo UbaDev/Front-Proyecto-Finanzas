e2029cad94f3ff12a376bae1fbad1c4a6b012e16
Ubaldo Aviles <2021171007@uteq.edu.mx>
2023-10-16 09:41:09 -0600

    .

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import styled from 'styled-components';
 import Sidebar from './components/Sidebar'
 import RighSidebar from './components/RightSidebar'
 import Dashboard from './components/Dashboard'
+import {LoginForm} from './components/LoginForm'
+import {RegisterForm} from './components/RegisterForm'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <Div>
-      <Router>
-      
+      <Router>  
         <Routes>
           <Route path="/" element={DashboardLayout()} />
-          {/* Login */}
-          {/* Registro */}
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </Router>
-      
     </Div>
-
-    
   );
 }
 
