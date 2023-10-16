5b160933d2aef25b921d82db788fca6a5fbc2ffa
Ubaldo Aviles <2021171007@uteq.edu.mx>
2023-10-16 09:37:08 -0600

    Rutas creadas, listo para implementar login y registro

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import styled from 'styled-components';
 import Sidebar from './components/Sidebar'
 import RighSidebar from './components/RightSidebar'
 import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
     <Div>
+      <Router>
+      
+        <Routes>
+          <Route path="/" element={DashboardLayout()} />
+          {/* Login */}
+          {/* Registro */}
+        </Routes>
+      </Router>
+      
+    </Div>
+
+    
+  );
+}
+
+function DashboardLayout() {
+  return (
+    <>
       <Sidebar />
       <Dashboard />
       <RighSidebar />
-    </Div>
+    </>
   );
 }
 
